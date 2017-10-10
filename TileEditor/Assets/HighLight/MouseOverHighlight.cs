a103d2ba753a3a7d748c53cf84725570d1987f3b
pplluumm <bunnydolphins_6@yahoo.com>
2017-10-10 11:47:45 -0700

    HighLighter
    A Script that puts a sprite above the others and renders it, the sprite is an empty box so it looks like the edges are highlighted.

---
+++ TileEditor/Assets/HighLight/MouseOverHighlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class MouseOverHighlight : MonoBehaviour
+{
+
+    public Color highlightColor = Color.yellow;
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = highlightColor;
+        spriteRenderer.sortingOrder = 1;
+        spriteRenderer.enabled = false;
+    }
+
+    void OnMouseEnter()
+    {
+        spriteRenderer.enabled = true;
+    }
+    void OnMouseExit()
+    {
+        spriteRenderer.enabled = false;
+    }
+}
