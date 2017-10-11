64e282b1e0f571ef1fada63f19be3077c400f3a4
Jordan Betcher <jordan.betcher@bellevuecollege.edu>
2017-10-11 07:22:19 -0700

    Raycast2D with mouse

---
+++ TileEditor/Assets/Raycast/FindWithRaycast.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FindWithRaycast : MonoBehaviour
+{
+    public Color highlightColor = Color.yellow;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = highlightColor;
+        spriteRenderer.sortingOrder = 1;
+        spriteRenderer.enabled = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float mouseY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+        Vector2 rayPosition = new Vector2(mouseX, mouseY);
+        RaycastHit2D hit = Physics2D.Raycast(rayPosition, Vector2.zero, 0f);
+
+        if (hit)
+        {
+            Debug.Log(hit.transform.name);
+            this.transform.position = hit.transform.position;
+            spriteRenderer.enabled = true;
+
+        }
+        else
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
+}
---
+++ TileEditor/Assets/Raycast/MouseHighlightedObject.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class MouseHighlightedObject : MonoBehaviour {}
