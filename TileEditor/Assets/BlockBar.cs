6affbc54a375dde2ce85d87cfe679f22891ec6ed
Jordan Betcher <jordan.betcher@bellevuecollege.edu>
2017-10-12 14:16:50 -0700

    BlockBar get Player
    Script that get's the root transform and checks if it is the player.

---
+++ TileEditor/Assets/BlockBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBar : MonoBehaviour
+{
+    Transform player;
+
+	void Start ()
+    {
+        player = transform.root;
+
+        if(player.tag != "Player")
+        {
+            Debug.Log("The root transform of " + transform.name + " must have a tag of Player");
+        }
+
+        Debug.Log(player.name);
+
+	}
+
+	void Update () {
+		
+	}
+}
